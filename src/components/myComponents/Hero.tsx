f3cdcfa848eae03b15a7a74378662d80ac6c3a3c
ayush <ayushmehrotra099@gmail.com>
2024-08-23 22:24:57 +0530

    added nothing

--- src/components/myComponents/Hero.tsx
+++ src/components/myComponents/Hero.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import Typed from "@/Utils/Typed";
 import { FaArrowRight } from "react-icons/fa";
+import Image from "next/image";
+
 const Hero = () => {
   return (
     <main className="flex text-white max-h-screen flex-col items-center justify-between p-24 overflow-hidden  ">
       <h1 className="text-8xl m-4 font-  ">
-        An <span>AI</span>
+        An <span>AI</span>{" "}
+        <Image src="/hero_paint.png" width={100} height={100} alt="hero" />
       </h1>
       <h1 className="text-8xl mt-2 text-center">
         <Typed />
